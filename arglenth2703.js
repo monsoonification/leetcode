e619b67876027ba828e490d0a6c7b1988631be47
monsoonification <austraeohperhaps@gmail.com>
2024-05-12 15:24:13 -0400

    this one is very dumb

---
+++ arglenth2703.js
@@ -0,0 +1,16 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
+function main() {
+    console.log(argumentsLength(1, 2, 3)); // 3
+}
+
+main();
