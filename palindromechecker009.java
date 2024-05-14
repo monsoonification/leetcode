aeba872652fbeb01721c5b982ab56c62db91924a
monsoonification <austraeohperhaps@gmail.com>
2024-05-14 11:19:06 -0400

    baby's first try leetcode execercise

---
+++ palindromechecker009.java
@@ -0,0 +1,27 @@
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        String xString = Integer.toString(x);
+        char[] testArray = xString.toCharArray();
+        int midpoint = Math.floorDiv(xString.length(), 2);
+        for (int i = 0; i < testArray.length; i++) {
+            if (testArray[i] != testArray[xString.length() - i - 1]) {
+                return false;
+            }
+            if (i == midpoint) {
+                break;
+            }
+        }
+        return true;
+        
+    }
+}
+
+public class palindromechecker009{
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.isPalindrome(121));
+        System.out.println(solution.isPalindrome(-121));
+        System.out.println(solution.isPalindrome(10));
+    }
+}
